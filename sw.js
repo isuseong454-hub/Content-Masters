c4ff27f95134bb429401994fa9b0e69e3f432c1a
isuseong454-hub <isuseong454@gmail.com>
2026-07-26 11:16:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker — 컨텐츠 마스터즈
 // 오프라인 작동 + PWA 설치 트리거
 
-const CACHE_NAME = 'content-masters-v499';
+const CACHE_NAME = 'content-masters-v502';
 const FILES = [
   './',
   './index.html',
